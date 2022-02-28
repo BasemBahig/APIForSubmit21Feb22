e9e6a45e7b749c9453a6ab5bfba2025f31959f04
Basem Hennawi <basembahig@gmail.com>
2022-03-01 01:26:45 +0200

    Add fsPromises

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@
   export default myFunc;*/
 import express from 'express';
 import routes from './routes/index'; 
+import fspromises from 'fs/promises';
 
   //import routes from './index';
   //routes.use('/api',routes);
+  const fs = require('fs').promises;
+
 const app = express();
 const port =3100;
 app.use('/api', routes);
@@ -15,8 +18,15 @@ app.listen(port, ()=>{
     console.log(`Server is up and running on http://localhost:${port}`)
 });
 
+//Use .open method with W+ also a+ to create resized image if doesn't exist
+
+const writeData = async ()=>{
+  const myFile = await fspromises.open(
+    'writeFile.txt', 'a+'
+  );
+};
 //request imge
-/*app.get('./src/api/v1/img/encenadaport.jpg', (req, res)=>{
+/*app.get('./src/api/v1/img/encenadaport.jpg', (req, res)=>
   console.log(req.query.image);
   res.sendStatus(200).send('good');
   //res.sendFile(Image);
